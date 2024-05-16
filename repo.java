e1e7d05aec7356acfe26a691d8669e8e98c6b401
sukanya-rathod <165880641+sukanya-rathod@users.noreply.github.com>
2024-05-16 06:47:06 +0530

    Update repo.java

--- repo.java
+++ repo.java
@@ -1,5 +1,6 @@
 pubic class repo{
     public static void main(String[] args) {
-        System.out.println("this is a single line print statement");
+        System.out.println("this is a first line print statement");
+        System.out.println("this is a second line print statement");
     }
 }
